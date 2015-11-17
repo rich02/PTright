f131a01019461bffc144f581e36f0e598a1349a1
rmixv8 <rmixv8@mail.missouri.edu>
2015-11-17 11:32:30 -0600

    added pictures to Add Exercise Collection View

---
+++ PTRight/ExerciseCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExerciseCollectionViewCell.swift
+//  PTRight
+//
+//  Created by Ryan Iffrig on 11/17/15.
+//  Copyright © 2015 James Tapia. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    
+}
--- PTRight/NewExerciseViewController.swift
+++ PTRight/NewExerciseViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class NewExerciseViewController: UIViewController {
-
+class NewExerciseViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var images: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        images = ["ankle_exercise.jpg", "foot_exercise.jpg", "wrist_twist.jpg",]
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +25,17 @@ class NewExerciseViewController: UIViewController {
     }
     
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! ExerciseCollectionViewCell
+        cell.imgView.image = UIImage(named: images[indexPath.row])
+        
+        return cell
+        
+           }
     /*
     // MARK: - Navigation
 
